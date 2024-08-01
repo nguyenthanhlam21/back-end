dfb704e8d7b74168358e7e949178de3c34c95e29
nguyenthanhlam21 <lamn47593@gmail.com>
2024-08-01 18:41:10 +0700

    add port

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.use(
   })
 );
 app.use(express.json());
+const port = process.env.PORT || 3000;
 
 const dbUrl = process.env.DB_URI || "mongodb+srv://lamn47593:n9e4uF8xW6Pa4dj8@cluster0.kafkaut.mongodb.net/Xuong_react";
 
 connectMongoDB(dbUrl);
 
 app.use("/", router);
-
-export const viteNodeApp = app;
+app.listen(port, () => console.log("Server is running with " + port));
+// export const viteNodeApp = app;
